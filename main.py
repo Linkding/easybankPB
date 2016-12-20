5c4a0d6fed7466d546a9aedd62e45bc6e9893b5e
LinKding <Linkding@linxiaobindeMacBook.local>
2016-12-20 08:42:41 +0800

    更新港銀股信息

--- main.py
+++ main.py
@@ -26,8 +26,9 @@ def change_dataframe():
             x_1 = [x.strip().split(' ')[1] for x in open(filename1).readlines()]
             x_2 = [x.strip().split(' ')[2] for x in open(filename1).readlines()]
             x_3 = [x.strip().split(' ')[3] for x in open(filename1).readlines()]
-            frame = list(zip(x_0,x_1,x_2,x_3))
-            df = pd.DataFrame(data = frame, columns=['codenumber', 'Date','bookvalue','changerate'])
+            x_4 = [x.strip().split(' ')[4] for x in open(filename1).readlines()]
+            frame = list(zip(x_0,x_1,x_2,x_3,x_4))
+            df = pd.DataFrame(data = frame, columns=['Codenumber','Name','Date','bookvalue','changerate'])
             df.to_csv(filename2,index=False,header=True)
 
 ## merge two table(csv) to one result csv
@@ -55,9 +56,13 @@ def merge():
                     num2 = index_num[i2][0]
                     if i == 0:
                         merge.loc[0:num1,'bookvalue'] = merge['bookvalue'][num1]
+                        merge.loc[0:num1,'Name'] = merge['Name'][num1]
+                        merge.loc[0:num1,'Codenumber'] = merge['Codenumber'][num1]
                         merge.loc[0:num1,'changerate'] = merge['changerate'][num1]
                     else:
                         merge.loc[num2:num1,'bookvalue'] = merge['bookvalue'][num1]
+                        merge.loc[0:num1,'Name'] = merge['Name'][num1]
+                        merge.loc[0:num1,'Codenumber'] = merge['Codenumber'][num1]
                         merge.loc[num2:num1,'changerate'] = merge['changerate'][num1]
 
                 merge['PB'] = merge['Close'] /( merge['bookvalue'] / merge['changerate'])
@@ -70,13 +75,14 @@ def collect_pb():
             for i in f.read().splitlines():
                 filename1 = 'merge' + i + '.csv'
                 stockdata = pd.read_csv(filename1)
-                line = stockdata[['Date','codenumber','name','Close','bookvalue','PB']][:1]
+                line = stockdata[['Codenumber','Name','Date','bookvalue','changerate','PB']][:1]
                 frame.append(line)
                 result = pd.concat(frame)
             result.sort_values('PB').to_csv('hk_pb.csv',index=False,header=True)
 
 
 if __name__ ==  "__main__":
-        collect()
-        change_dataframe()
+#        collect()
+#        change_dataframe()
         merge()
+        collect_pb()
